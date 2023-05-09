8bd5a971c4e7faafec8643a684f88fb005773c7a
Kenneth Bingham <w@qrk.us>
2023-05-09 12:48:00 -0400

    update

--- setuidtest.c
+++ setuidtest.c
@@ -9,9 +9,34 @@
 #include <string.h>
 #include <sys/capability.h>
 
+void print_caps() {
+    cap_t caps;
+    char *text_caps;
+
+    /* Get the current process capabilities */
+    cap_init();
+    caps = cap_get_proc();
+    if (caps == NULL) {
+        perror("cap_get_proc");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Print the capabilities */
+    text_caps = cap_to_text(caps, NULL);
+    printf("Process capabilities:\n");
+    printf("%s\n", text_caps);
+
+    /* Free the capability set */
+    if ( cap_free(text_caps) != 0 || cap_free(caps) != 0 ) {
+        perror("cap_free");
+        exit(EXIT_FAILURE);
+    }
+}
+
 int main (int argc, char *argv[]) {
     system("/usr/bin/id");
     system("/bin/grep '^Cap' /proc/self/status");
+    print_caps();
     struct passwd *pw;
     // errno = 0;
     pw = getpwnam("ziti");
@@ -26,4 +51,5 @@ int main (int argc, char *argv[]) {
     system("/usr/bin/env");
     system("/usr/bin/id");
     system("/bin/grep '^Cap' /proc/self/status");
+    print_caps();
 }
